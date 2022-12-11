9af4b6a93c1851ec685664c6b373168537f5b7f6
Jullian0426 <jullian0426@yahoo.com>
2022-12-11 07:36:22 -0600

    Create prompt for length that checks for invalid answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,35 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", prompts);
+
+
 var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789\!\"\#\$\%\&\'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\\\]\^\_\`\{\|\}\~';
 var result = '';
 
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", generatePassword);
+// Give user prompts for password criteria
+function prompts() {
+  // Ask user for password length
+  var length = window.prompt("Set the Password Length. At least 8 but no more than 128.")
+  
+  // Test if user input is valid
+  const isANumber = /^\d+$/.test(length);
+  if (isANumber && length >= 8 && length <= 128) {
+    window.alert("is a number")
+  } else {
+    window.alert("Please Try Again")
+    return;
+  }
+  
+  var lowercase = window.confirm("Include lowercase? OK for yes, Cancel for no.")
 
-// Generates Password when button is clicked
+  // Ask user what characters to include
+}
+
+
+// Randomly generate a string
 function generatePassword(length) {
 
   for ( var i = 0; i < length; i++ ) {
@@ -21,4 +42,3 @@ function generatePassword(length) {
 
 }
 
-generatePassword(10);
