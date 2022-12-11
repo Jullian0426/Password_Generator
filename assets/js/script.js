3da8ae2dd223dd280ffd56d32a50990d01132d88
Jullian0426 <jullian0426@yahoo.com>
2022-12-11 05:55:57 -0600

    Move 'characters' and 'result' variables to global scope, and combine writePassword with generatePassword

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,24 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789\!\"\#\$\%\&\'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\\\]\^\_\`\{\|\}\~';
+var result = '';
 
-  passwordText.value = password;
-
-}
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", generatePassword);
 
+// Generates Password when button is clicked
 function generatePassword(length) {
-  var result           = '';
-  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789\!\"\#\$\%\&\'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\\\]\^\_\`\{\|\}\~';
+
   for ( var i = 0; i < length; i++ ) {
       result += characters.charAt(Math.floor(Math.random() * characters.length));
   }
-  return result;
+
+  // Write password to the #password input
+  var passwordText = document.querySelector("#password");
+  passwordText.value = result;
+
 }
 
-console.log(generatePassword(5));
+generatePassword(10);
