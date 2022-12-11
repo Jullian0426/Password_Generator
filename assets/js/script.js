a126af6bc8a35767f8e1ac32b45ae3fd5c707853
Jullian0426 <jullian0426@yahoo.com>
2022-12-11 08:29:28 -0600

    Fix splicing, turn characters array into string, display error when when no characters are selected, merge generatePassword function with prompts function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,11 @@
 var generateBtn = document.querySelector("#generate");
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", prompts);
-
-
-
+generateBtn.addEventListener("click", generatePassword);
 
 
 // Give user prompts for password criteria
-function prompts() {
+function generatePassword() {
   var characters = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz', '0123456789', '\!\"\#\$\%\&\'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\\\]\^\_\`\{\|\}\~'];
   var result = '';
 
@@ -18,43 +15,44 @@ function prompts() {
   
   // Test if user input is valid
   const isANumber = /^\d+$/.test(length);
-  if (isANumber && length >= 8 && length <= 128) {
-    window.alert("is a number")
-  } else {
+  if (!isANumber || length <= 8 || length >= 128) {
     window.alert("Please Try Again")
     return;
   }
   
   // Ask user what characters to include
-  var uppercase = window.confirm("Include lowercase? OK for yes, Cancel for no.")
+  var uppercase = window.confirm("Include uppercase? OK for yes, Cancel for no.")
   if (!uppercase) {
-    characters.splice(0, 1);
+    characters.splice(0, 1, '');
   }
 
   var lowercase = window.confirm("Include lowercase? OK for yes, Cancel for no.")
   if (!lowercase) {
-    characters.splice(1, 1);
+    characters.splice(1, 1, '');
   }
 
-  var numbers = window.confirm("Include lowercase? OK for yes, Cancel for no.")
+  var numbers = window.confirm("Include numbers? OK for yes, Cancel for no.")
   if (!numbers) {
-    characters.splice(2, 1);
+    characters.splice(2, 1, '');
   }
 
-  var specials = window.confirm("Include lowercase? OK for yes, Cancel for no.")
+  var specials = window.confirm("Include special characters? OK for yes, Cancel for no.")
   if (!specials) {
-    characters.splice(3, 1);
+    characters.splice(3, 1, '');
   }
 
+  //Join all characters together in one string
+  characters = characters.join('')
 
-}
+  //Display error if no character types are selected
+  if (characters === '') {
+    window.alert("Please Try Again")
+    return;
+  }
 
-
-// Randomly generate a string
-function generatePassword(length) {
-
+  // Randomly generate a string based on user input
   for ( var i = 0; i < length; i++ ) {
-      result += characters.charAt(Math.floor(Math.random() * characters.length));
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
   }
 
   // Write password to the #password input
